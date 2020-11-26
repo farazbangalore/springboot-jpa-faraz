bbad83fc61b50953bb3591dece27e1d5bc2ad06a
Faraz Hussain <faraz.hussain@epsilon.com>
2020-11-26 15:11:48 +0530

    First Commit for this branch

---
+++ src/main/java/com/epsilon/springboot/mvc/SpringbootMvcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.epsilon.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsilon/springboot/mvc/SpringbootMvcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsilon.springboot.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
